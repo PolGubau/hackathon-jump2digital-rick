8fc5066d721bce8043436bfacc276472c3d603e0
Pol Gubau Amores <gubaupol@gmail.com>
2023-10-15 23:11:09 +0200

    Pol Gubau; MW-Correcting state names

--- src/states/index.ts
+++ src/states/index.ts
@@ -1,5 +1,5 @@
 export * from './filters_state';
-export { default as language.state } from './language.state';
-export { default as modal.state } from './modal.state';
-export { default as theme.state } from './theme.state';
-export { default as toast.state } from './toast.state';
+export { default as languageState } from './language.state';
+export { default as modalState } from './modal.state';
+export { default as themeState } from './theme.state';
+export { default as toastState } from './toast.state';
