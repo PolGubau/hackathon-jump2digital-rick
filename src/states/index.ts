edb21b1d5f0190535030a4dbd10c1e3ee276b92d
Pol Gubau Amores <gubaupol@gmail.com>
2023-10-15 23:10:50 +0200

    Pol Gubau; MW-Correcting state importations

--- src/states/index.ts
+++ src/states/index.ts
@@ -1,2 +1,5 @@
-export { default as modalState } from "./modal.state";
-export { default as toastState } from "./toast.state";
+export * from './filters_state';
+export { default as language.state } from './language.state';
+export { default as modal.state } from './modal.state';
+export { default as theme.state } from './theme.state';
+export { default as toast.state } from './toast.state';
